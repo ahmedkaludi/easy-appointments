53aab17c8556dcb11420036d56e652fb8ccbfe35
Nikola Loncar <nikolanbg@gmail.com>
2019-08-12 19:29:24 +0200

    Version bump 2.5.7

--- trunk/main.php
+++ trunk/main.php
@@ -4,7 +4,7 @@
  * Plugin Name: Easy Appointments
  * Plugin URI: https://easy-appointments.net/
  * Description: Simple and easy to use management system for Appointments and Bookings
- * Version: 2.5.6
+ * Version: 2.5.7
  * Author: Nikola Loncar
  * Author URI: http://nikolaloncar.com
  * Text Domain: easy-appointments
@@ -20,7 +20,7 @@ if (!defined('WPINC')) {
 /**
  * Currently plugin version.
  */
-define( 'EASY_APPOINTMENTS_VERSION', '2.5.5' );
+define( 'EASY_APPOINTMENTS_VERSION', '2.5.7' );
 
 // path for source files
 define('EA_SRC_DIR', dirname(__FILE__) . '/src/');
