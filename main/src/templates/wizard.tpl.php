02ac2ef73edfea3f1cfca7202804c8c77ff91d08
Milos Petrovic <milos.petrovic@esome.com>
2020-04-18 13:36:50 +0200

    #25 Added frames for steps

--- main/src/templates/wizard.tpl.php
+++ main/src/templates/wizard.tpl.php
@@ -1,25 +1,41 @@
 <template id="ea-wizard-template">
-    <div class="ea-wizard-overlay">
-        <div class="ea-wizard">
-            <div class="ea-wizard-header">
-                <h3>TITLE</h3>
-                <div class="ea-wizard-progress"></div>
-            </div>
-            <div class="ea-wizard-content">
-                <div class="ea-wizard-step current ea-loading">LOADING...</div>
-                <div class="ea-wizard-step ea-step-location">LOCATION</div>
-                <div class="ea-wizard-step ea-step-worker">WORKER</div>
-                <div class="ea-wizard-step ea-step-service">SERVICE</div>
-                <div class="ea-wizard-step ea-step-calendar">CALENDAR</div>
-                <div class="ea-wizard-step ea-step-timeslots">SLOTS</div>
-                <div class="ea-wizard-step ea-step-personal-info">PERSONAL INFO</div>
-                <div class="ea-wizard-step ea-step-overview">OVERVIEW</div>
-                <div class="ea-wizard-step ea-step-thanks">THANKS</div>
-            </div>
-            <div class="ea-wizard-actions">
-                <button class="ea-wizard-prev" style="display: none">Prev</button>
-                <button class="ea-wizard-next">Next</button>
-            </div>
-        </div>
+  <div class="ea-wizard-overlay">
+    <div class="ea-wizard">
+
+      <div class="ea-wizard-header">
+        <h3>Create an appointment</h3>
+      </div>
+
+      <div class="ea-wizard-progress">
+        <ul class="ea-wizard-progress-steps">
+          <li class="ea-wizard-progress-step current" id="location"><span class="icon-location"></span>Location</li>
+          <li class="ea-wizard-progress-step" id="worker">Worker</li>
+          <li class="ea-wizard-progress-step" id="service">Service</li>
+          <li class="ea-wizard-progress-step" id="calendar">Calendar</li>
+          <li class="ea-wizard-progress-step" id="slots">Slots</li>
+          <li class="ea-wizard-progress-step" id="personal">Personal Info</li>
+          <li class="ea-wizard-progress-step" id="overview">Overview</li>
+          <li class="ea-wizard-progress-step" id="thanks">Thanks</li>
+        </ul>
+      </div>
+
+      <div class="ea-wizard-content">
+        <!--<div class="ea-wizard-step current ea-loading">LOADING...BRE</div>-->
+        <div class="ea-wizard-step ea-step-location current">LOCATION</div>
+        <div class="ea-wizard-step ea-step-worker">WORKER</div>
+        <div class="ea-wizard-step ea-step-service">SERVICE</div>
+        <div class="ea-wizard-step ea-step-calendar">CALENDAR</div>
+        <div class="ea-wizard-step ea-step-timeslots">SLOTS</div>
+        <div class="ea-wizard-step ea-step-personal-info">PERSONAL INFO</div>
+        <div class="ea-wizard-step ea-step-overview">OVERVIEW</div>
+        <div class="ea-wizard-step ea-step-thanks">THANKS</div>
+      </div>
+
+      <div class="ea-wizard-actions">
+        <button class="ea-wizard-prev" style="display: none">Prev</button>
+        <button class="ea-wizard-next">Next</button>
+      </div>
+
     </div>
+  </div>
 </template>
