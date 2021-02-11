7907db68e0a750da5bc5c2d3af87415197b0bbe4
Milos Petrovic <milos.petrovic@esome.com>
2021-02-11 23:34:26 +0100

    #84 Almost done

--- main/js-react/src/ea-components/FormFields/MultiFieldWrap/index.js
+++ main/js-react/src/ea-components/FormFields/MultiFieldWrap/index.js
@@ -1,12 +1,23 @@
 import React from 'react';
 
-import FieldSet from '../FieldSet';
+import InfoOutlinedIcon from '@material-ui/icons/InfoOutlined';
 
-const MultiFieldWrap = ({ label, children, className }) => (
-  <div className="ea-form-field">
-    <FieldSet label={label} className={`multiwrap ${className ?? ''}`}>
+const MultiFieldWrap = ({ label, children, className, info }) => (
+  <div className={`ea-multi-wrap ${className ?? ''}`}>
+    <div className="ea-multi-wrap__content">
+      {info && (
+        <div className="d-flex align-items-center mb-3">
+          <InfoOutlinedIcon className="ea-info-md text-black-50" />
+          <span className="ea-info-sm ml-1">{info}</span>
+        </div>
+      )}
       {children}
-    </FieldSet>
+    </div>
+    <fieldset className="ea-multi-wrap__fieldset">
+      <legend className="ea-multi-wrap__label">
+        <span>{label}</span>
+      </legend>
+    </fieldset>
   </div>
 );
 
