bf323ee1c1bcc10ca5b84e4148407e79a4065736
Milos Petrovic <milos.petrovic@esome.com>
2021-05-16 20:41:48 +0200

    #86 Added calendar - initial implementation

--- main/js-react/src/pages/Reports/components/TimeTable/index.js
+++ main/js-react/src/pages/Reports/components/TimeTable/index.js
@@ -1,3 +1,32 @@
 import React from 'react';
 
-export const TimeTable = () => <div>Time Table</div>;
+import moment from 'moment';
+import { Calendar, momentLocalizer } from 'react-big-calendar';
+
+import 'react-big-calendar/lib/css/react-big-calendar.css';
+
+const localizer = momentLocalizer(moment);
+
+export const TimeTable = () => {
+  const events = [
+    {
+      start: moment().toDate(),
+      end: moment()
+        .add(1, 'days')
+        .toDate(),
+      title: 'Some title'
+    }
+  ];
+
+  return (
+    <div>
+      <Calendar
+        localizer={localizer}
+        defaultDate={new Date()}
+        defaultView="month"
+        events={events}
+        style={{ height: '100vh' }}
+      />
+    </div>
+  );
+};
